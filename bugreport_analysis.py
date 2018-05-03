38d9a40e569259002fab7e815357da5218da6f05
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-05-03 12:01:21 +0530

    bureport add PLOGE support

--- bugreport_analysis.py
+++ bugreport_analysis.py
@@ -19,7 +19,7 @@ TODO:
 4. Prepare CLI to shor the data as per user request
 5. Add CLI option to short data with default configuration
 
-Structure: 
+Structure:
 
 bugreport_analysis/
 |-- build_details.txt
@@ -31,15 +31,16 @@ command:
 
 '''
 
-#------------------------
+# ------------------------
 # this
-#-----------------------
+# -----------------------
 
 # get options object and all config
 OPT = util.OPT
 debug_enable = config.MODE_DEBUG
 test_enable = config.MODE_TEST
-TAG  = 'bugreport_analysis'
+TAG = 'bugreport_analysis'
+
 
 def setup_ws():
     try:
@@ -47,52 +48,55 @@ def setup_ws():
             shutil.rmtree(util.ws_out)
         os.makedirs(util.ws_out)
     except os.error as err:
-        util.PLOGE(TAG,str(err),exit=False)
+        util.PLOGE(TAG, str(err), exit=False)
         return False
     return True
-    
-        
 
 
 def prepare_bugreport_raw_data():
     if not setup_ws():
-        util.PLOGE(TAG,'failed to setup ws',exit=False)
-        return False
-    
-    is_unzip_required, error = util.is_unzip_required(OPT.zip_file)
-    
-    if error:
-        util.PLOGE(TAG,'bugreport file type wrong, expected TEXT or ZIP ',exit=False)
+        util.PLOGE(TAG, 'failed to setup ws', exit=False)
         return False
 
-    sys.path 
+    is_unzip_required, error = util.is_unzip_required(OPT.zip_file)
+
+    if error:
+        util.PLOGE(
+            TAG, 'bugreport file type wrong, expected TEXT or ZIP ', exit=False)
+        return False
+
+    sys.path
     if is_unzip_required:
         print ' Extracting ...'
-        with zipfile.ZipFile(OPT.zip_file,'r') as bug_zip:
+        with zipfile.ZipFile(OPT.zip_file, 'r') as bug_zip:
             try:
                 bug_zip.extractall(util.ws_out)
             except zipfile.BadZipfile:
-                util.PLOGE(TAG,'Badzipfile',exit=False)
+                util.PLOGE(TAG, 'Badzipfile', exit=False)
                 return False
             except zipfile.LargeZipFile:
-                util.PLOGE(TAG,'LargeZipFile',exit=False)
-                return False     
+                util.PLOGE(TAG, 'LargeZipFile', exit=False)
+                return False
     return True
+
 
 def check_prerequisite():
     if OPT.file_name is not None:
         OPT.zip_file = os.path.abspath(OPT.file_name)
         if not os.path.isfile(OPT.zip_file):
-            util.PLOGE(TAG,'File not found or is not a file : ', OPT.zip_file ,exit=False)
+            util.PLOGE(TAG, 'File not found or is not a file : ',
+                       OPT.zip_file, exit=False)
             return False
-       
+
         is_unzip_required, error = util.is_unzip_required(OPT.zip_file)
-        
+
         if error:
-            util.PLOGE(TAG,'bugreport file type wrong, expected TEXT or ZIP ',exit=False)
+            util.PLOGE(
+                TAG, 'bugreport file type wrong, expected TEXT or ZIP ', exit=False)
             return False
-        
+
     return True
+
 
 def usage():
     util.print_empty_line()
@@ -105,48 +109,50 @@ def usage():
     print '\t--version\t\t - print version'
     util.print_empty_line()
 
+
 def parse_argument(argv):
-    long_opts = ['help','version','verbose','file=']
+    long_opts = ['help', 'version', 'verbose', 'file=']
     short_opts = 'hvl'
-    
-    try : 
-        opts_list, args_pos = getopt.getopt(argv[1:],short_opts,long_opts)
+
+    try:
+        opts_list, args_pos = getopt.getopt(argv[1:], short_opts, long_opts)
     except getopt.GetoptError:
         util.print_empty_line()
         print 'Error : args parser '
-        usage()  
+        usage()
         sys.exit(1)
 
     if debug_enable:
-        util.PLOGD(TAG,'opts are :',str(opts_list))
-        util.PLOGD(TAG,'args are :',str(args_pos))
+        util.PLOGD(TAG, 'opts are :', str(opts_list))
+        util.PLOGD(TAG, 'args are :', str(args_pos))
 
     if args_pos:
         usage()
         sys.exit(-1)
 
-    for opt,val in opts_list:
+    for opt, val in opts_list:
         if opt == '--file':
             util.OPT.file_name = val
-        elif opt in ['-h','--help']:
+        elif opt in ['-h', '--help']:
             usage()
             sys.exit(0)
         elif opt == '--version':
             print util.get_version()
             sys.exit(0)
-        elif opt in ['-v','--verbose']:
+        elif opt in ['-v', '--verbose']:
             util.OPT.verbose = True
         else:
             print 'Error: wrong option : ' + opt
             sys.exit(0)
 
+
 def start_analysis():
-    # check cmd line args     
+    # check cmd line args
     if not check_prerequisite():
         usage()
-        util.PLOGE(TAG,'check prerequitsite failed',exit=True)
+        util.PLOGE(TAG, 'check prerequitsite failed', exit=True)
     if not prepare_bugreport_raw_data():
-        util.PLOGE(TAG,'Prepare bugreport data failed',exit=True)
+        util.PLOGE(TAG, 'Prepare bugreport data failed', exit=True)
 
 
 def main():
@@ -154,7 +160,6 @@ def main():
     parse_argument(sys.argv)
     start_analysis()
 
+
 if __name__ == '__main__':
     main()
-
-
