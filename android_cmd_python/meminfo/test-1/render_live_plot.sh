9f82ba020a42ea62ea49cd69b1b98cc3bf4d822c
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-07-24 15:54:56 +0530

    [android_cmd_python] : Clean files

---
+++ android_cmd_python/meminfo/test-1/render_live_plot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$1" == "-i" ]; then
+    file=liveplot_ion_meminfo.gnu
+fi
+
+if [ "$1" == "-m" ]; then
+    file=liveplot_meminfo.gnu
+fi
+
+while [ 1 ] ; do
+gnuplot $file
+sleep 1
+
+done
